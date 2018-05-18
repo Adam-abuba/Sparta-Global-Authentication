f861d5104717ade775bd0be7cf179418061bafe6
Tech-A02 <tech-a02@Tech-A02s-MacBook-Air.local>
2018-05-18 09:53:36 +0100

    Authorization addedd

---
+++ DeviseLesson/app/policies/post_policy.rb
@@ -0,0 +1,11 @@
+class PostPolicy < ApplicationPolicy
+  attr_reader :user, :post
+
+  def initialize(user, post)
+    @user = user
+    @post = post
+  end
+  def destroy?
+  user.admin?
+  end
+end
